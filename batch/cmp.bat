df52dc79172013e60a7c3950830767814482484f
andricfilip2000@gmail.com <andricfilip2000@gmail.com>
2020-07-11 20:58:01 +0200

    Final version

---
+++ batch/cmp.bat
@@ -0,0 +1,4 @@
+@echo off
+cd c:\bc31\bin
+bcc +C:\bc31\BIN\vscode\batch\cmpsw.txt %1 APPLICAT.LIB
+@echo on
---
+++ batch/dbg.bat
@@ -0,0 +1,4 @@
+@echo off
+cd c:\bc31\bin
+td.exe -ctdconfig.td -sdC:\bc31\BIN\vscode\src C:\bc31\BIN\vscode\output\vs_test.exe
+@echo on
